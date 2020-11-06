ff59966c337d45cf0c4e5138a3d65e548775e8c2
M <magic.ru789@gmail.com>
2020-11-06 12:47:09 -0600

    Support partial payments and top up payments for api access

--- util/eth_payments.py
+++ util/eth_payments.py
@@ -3,6 +3,27 @@ import time
 import datetime
 from web3 import Web3
 from database.models import commit, db_session, select, Payment
+from util import get_eth_amount, min_payment_amount_tier1, min_payment_amount_tier2
+
+default_api_calls_count = 6000000
+
+
+def calc_api_calls_tiers(payment_amount_wei, tier1_eth_amount, tier2_eth_amount,
+                         archival_mode: bool, def_api_calls_count: int) -> int:
+    """Calculates the number of api calls for the specified archival mode and tier
+    amounts. The [default api call count] * [price multiplier] determines total paid
+    api calls. [price multiplier] = [user payment in eth] / [tier required payment in eth]"""
+    tier_expected_amount = tier1_eth_amount if not archival_mode else tier2_eth_amount
+    multiplier = float(Web3.fromWei(payment_amount_wei, 'ether')) / float(tier_expected_amount)
+    api_calls = int(float(def_api_calls_count) * multiplier)
+    return api_calls
+
+
+def calc_api_calls(payment_amount_wei, archival_mode: bool, def_api_calls_count: int) -> int:
+    tier1_eth_amount = get_eth_amount(min_payment_amount_tier1)
+    tier2_eth_amount = get_eth_amount(min_payment_amount_tier2)
+    return calc_api_calls_tiers(payment_amount_wei, tier1_eth_amount, tier2_eth_amount, archival_mode,
+                                def_api_calls_count)
 
 
 class Web3Helper:
@@ -25,8 +46,7 @@ class Web3Helper:
     @db_session
     def loop_accounts(self):
         while True:
-            query = select(p for p in Payment if p.pending is True
-                           and p.start_time < (datetime.datetime.now() + datetime.timedelta(hours=4)))
+            query = select(p for p in Payment if p.start_time is not None)
 
             query2 = query.filter(lambda payment: payment.address is not None)
 
@@ -51,23 +71,58 @@ class Web3Helper:
             tx_hash = tx['hash'].hex()
             to_address = tx['to']
             value = tx['value']
+            if value <= 0:
+                continue
 
             if to_address in self.accounts:
                 print('payment received: {} {} {}'.format(tx_hash, to_address, value))
 
                 payment_obj = Payment.get(address=to_address)
 
-                if value < payment_obj.tier1_expected_amount or value < payment_obj.tier2_expected_amount:
-                    payment_obj.project.active = False
+                # Supporting partial payments and handling expired payments:
+                # If initial price for default 6M calls is not expired use that to calculate number of calls
+                # from partial payments. If the initial price has expired, use the current price of eth to
+                # determine how many calls the user receives. First time partial payments default to non-archival
+                # api access. In order for users to obtain archival access their first payment must be greater
+                # than or equal to the minimum payment amount for archival access.
+                #
+                # Make sure db supports large enough integer to track used_api_tokens (i.e. int64)
+
+                # If the project has never been activated (payment is pending) allow the user to set the archival mode.
+                # Activated projects cannot change the archival state.
+                if payment_obj.pending:
+                    # Check if initial payment offering has expired
+                    if datetime.datetime.now() >= payment_obj.start_time + datetime.timedelta(hours=3, minutes=30):
+                        # Expired time requires obtaining new payment tier calcs
+                        tier2_expected_amount = get_eth_amount(min_payment_amount_tier2)
+                        payment_obj.project.archive_mode = value >= tier2_expected_amount
+                        # Note set the api calls here since first time payment (do not append)
+                        payment_obj.project.api_token_count = calc_api_calls(value, payment_obj.project.archive_mode,
+                                                                             default_api_calls_count)
+                    else:
+                        # Non-expired payment calcs should use the db payment tiers
+                        payment_obj.project.archive_mode = value >= payment_obj.tier2_expected_amount
+                        # Note set the api calls here since first time payment (do not append)
+                        payment_obj.project.api_token_count = calc_api_calls_tiers(value,
+                                                                                   payment_obj.project.archive_mode,
+                                                                                   default_api_calls_count,
+                                                                                   payment_obj.tier1_expected_amount,
+                                                                                   payment_obj.tier2_expected_amount)
                 else:
-                    payment_obj.project.active = True
+                    # Append api calls because this is a top-up payment (first payment already received)
+                    payment_obj.project.api_token_count += calc_api_calls(value, payment_obj.project.archive_mode,
+                                                                          default_api_calls_count)
 
-                if value >= payment_obj.tier2_expected_amount:
-                    payment_obj.project.archive_mode = True
-
+                payment_obj.project.active = True
                 payment_obj.pending = False
-                payment_obj.amount = value
-                payment_obj.tx_hash = tx_hash
+                if not payment_obj.amount:
+                    payment_obj.amount = value
+                else:
+                    payment_obj.amount += value
+                if not payment_obj.tx_hash:
+                    payment_obj.tx_hash = tx_hash
+                else:
+                    payment_obj.tx_hash += ',' + tx_hash
                 payment_obj.project.expires = datetime.datetime.now() + datetime.timedelta(days=30)
 
                 commit()
